32cfcb36c909cfd23d981a2ab400b1d4918ebbda
alex-bene <alexandrosbene@gmail.com>
2025-09-04 11:17:44 +0200

    rename `draw_annotations.py` and internal functions + options to not draw on the image.

--- src/tinytools/process_annotations.py
+++ src/tinytools/process_annotations.py
@@ -54,8 +54,13 @@ def _get_colormap(
     return [tuple(color) for color in ((colors * 255).astype(int) if colors.max() <= 1.0 else colors).tolist()]
 
 
-def draw_bboxes(
-    image: Image.Image, bboxes: np.ndarray | None, colors: np.ndarray | None = None, colormap: str = "tab10"
+def process_bboxes(
+    image: Image.Image,
+    bboxes: np.ndarray | None,
+    colors: np.ndarray | None = None,
+    colormap: str = "tab10",
+    draw_bboxes: bool = True,
+    crop_bboxes: bool = True,
 ) -> dict[str, Image.Image | list[Image.Image]]:
     """Draw bounding boxes on image and return bounding box crops.
 
@@ -66,45 +71,52 @@ def draw_bboxes(
             colors will be generated based on the `colormap`. Defaults to None.
         colormap (str, optional): Matplotlib colormap name for mask colors to use if `colors` is None.
             Defaults to "tab10".
+        draw_bboxes (bool, optional): Whether to return an image with drawn bounding boxes. Defaults to True.
+        crop_bboxes (bool, optional): Whether to return a cropped image for each bounding box. Defaults to True.
 
     Returns:
         dict[str, Image.Image | list[Image.Image]]: A dictionary containing the original image with bounding boxes drawn
-            and a list of cropped images for each bounding box.
+            if `draw_bboxes` is True, and a list of cropped images for each bounding box.
 
     """
-    full_image = image.copy()
+    results = {}
 
-    if bboxes is None:
-        return image.copy(), []
-
-    # Create a copy of the original image for drawing
-    draw = ImageDraw.Draw(full_image)
+    if bboxes is None or (not draw_bboxes and not crop_bboxes):
+        return results
 
     if bboxes.ndim != 2 or bboxes.shape[1] != 4:
         msg = f"Expected 2D tensor of shape (N, 4) for bboxes, got {bboxes.shape}"
         raise ValueError(msg)
 
+    if crop_bboxes:
+        results["cropped_bboxes"] = [image.crop(list(map(int, bbox))) for bbox in bboxes]
+
+    if not draw_bboxes:
+        return results
+
+    # Create a copy of the original image for drawing
+    full_image = image.copy()
+    draw = ImageDraw.Draw(full_image)
     # Generate colors from colormap
     colors = _get_colormap(len(bboxes), colors, colormap)
-
     # Get bbox width based on image size
     img_width, img_height = full_image.size
     bbox_width = max(1, min(img_width, img_height) // 250)
 
-    crops = []
     for i, bbox in enumerate(bboxes):
         draw.rectangle(list(map(int, bbox)), outline=colors[i], width=bbox_width)
-        crops.append(image.crop(list(map(int, bbox))))
 
-    return {"image": full_image, "cropped_bboxes": crops}
+    return {"image": full_image} | results
 
 
-def draw_masks(
+def process_seg_masks(
     image: Image.Image,
     masks: np.ndarray | None,
     colors: np.ndarray | None = None,
     colormap: str = "tab10",
     transparency: float = 0.5,
+    draw_masks: bool = True,
+    remove_background: bool = True,
 ) -> dict[str, Image.Image]:
     """Draw masks on image and remove background.
 
@@ -116,26 +128,27 @@ def draw_masks(
         colormap (str, optional): Matplotlib colormap name for mask colors to use if `colors` is None.
             Defaults to "tab10".
         transparency (float, optional): Transparency level of mask overlay (0-1). Defaults to 0.5.
+        draw_masks (bool, optional): Whether to return an image with drawn masks. Defaults to True.
+        remove_background (bool, optional): Whether to return an image with background removed. Defaults to True.
 
     Returns:
-        dict[str, Image.Image]: A dictionary containing the original image with masks drawn and the image with all
-            the regions apart from the masks (background) replaced with transparency.
+        dict[str, Image.Image]: A dictionary containing the original image with masks drawn if `draw_masks` is True, and
+            the image with all the regions apart from the masks (background) replaced with transparency.
 
     """
-    full_image = image.copy().convert("RGBA")
-    no_background = Image.new("RGBA", full_image.size, (0, 0, 0, 0))
+    if masks is None or (not draw_masks and not remove_background):
+        return {}
 
-    if masks is None:
-        return image.copy(), no_background
-
-    # Create a copy of the original image for drawing
-    image_mode = image.mode
-    full_image = full_image.convert("RGBA")
-    width, height = full_image.size
-
+    width, height = image.size
     if masks.ndim != 3 or masks.shape[1] != height or masks.shape[2] != width:
         msg = f"Expected 3D tensor of shape (N, {height}, {width}) for masks, got {masks.shape}"
         raise ValueError(msg)
+
+    image_mode = image.mode
+    if draw_masks:
+        full_image = image.copy().convert("RGBA")
+    if remove_background:
+        no_background = Image.new("RGBA", full_image.size, (0, 0, 0, 0))
 
     # Generate colors from colormap
     colors = _get_colormap(len(masks), colors, colormap)
@@ -145,12 +158,20 @@ def draw_masks(
         # Convert mask to PIL Image (assuming mask is 0-1 or 0-255)
         seg_arr = (((mask * 255) if mask.max() <= 1.0 else mask) * transparency).astype(np.uint8)
 
-        # Create binary mask image
-        seg_mask = Image.fromarray(seg_arr)
-        overlay_color = Image.new("RGBA", full_image.size, colors[i])
-        full_image = Image.composite(overlay_color, full_image, seg_mask)
+        if draw_masks:
+            # Create binary mask image
+            seg_mask = Image.fromarray(seg_arr)
+            overlay_color = Image.new("RGBA", full_image.size, colors[i])
+            full_image = Image.composite(overlay_color, full_image, seg_mask)
 
-        # Add region to no-background image
-        no_background = Image.composite(image, no_background, Image.fromarray(seg_arr.astype(bool)))
+        if remove_background:
+            # Add region to no-background image
+            no_background = Image.composite(image, no_background, Image.fromarray(seg_arr.astype(bool)))
 
-    return {"image": full_image.convert(image_mode), "no_background_image": no_background}
+    results = {}
+    if draw_masks:
+        results["image"] = full_image.convert(image_mode)
+    if remove_background:
+        results["no_background_image"] = no_background
+
+    return results
