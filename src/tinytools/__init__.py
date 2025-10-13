9c6e101bb5c8f260d9b26de0b409fa52d6ec3edc
alex-bene <alexandrosbene@gmail.com>
2025-10-13 17:52:04 +0200

    prettier tqdm w/ rich & pbar rate fix

--- src/tinytools/__init__.py
+++ src/tinytools/__init__.py
@@ -3,5 +3,6 @@ from .image import image_grid, img_from_array, imgs_from_array_batch
 from .litellm import LiteLLMModel
 from .logger import get_logger, setup_prettier_root_logger
 from .process_annotations import process_bboxes, process_seg_masks
+from .tqdm import setup_prettier_tqdm
 from .video import get_video_fps, load_video, save_video
 from .vllm import VLLMModel
---
+++ src/tinytools/tqdm.py
@@ -0,0 +1,48 @@
+"""Logger tools."""
+
+from __future__ import annotations
+
+import tqdm as tqdm_og
+from rich.progress import ProgressColumn, Task, Text, filesize
+from tqdm import rich as tqdm_rich
+
+tqdm_og.tqdm = tqdm_rich.tqdm
+
+
+class RateColumn(ProgressColumn):
+    """Renders human readable transfer speed."""
+
+    def __init__(self, unit: str = "", unit_scale: bool = False, unit_divisor: int = 1000) -> None:
+        self.unit = unit
+        self.unit_scale = unit_scale
+        self.unit_divisor = unit_divisor
+        super().__init__()
+
+    def render(self, task: Task) -> Text:
+        """Show data transfer speed."""
+        speed = task.speed
+        if speed is None:
+            return Text(f"? {self.unit}/s", style="progress.data.speed")
+        if self.unit_scale:
+            unit, suffix = filesize.pick_unit_and_suffix(
+                speed, ["", "K", "M", "G", "T", "P", "E", "Z", "Y"], self.unit_divisor
+            )
+            precision = 0 if unit == 1 else 1
+        else:
+            unit, suffix = filesize.pick_unit_and_suffix(speed, [""], 1)
+            precision = 1
+        return (
+            Text(f"{speed / unit:,.{precision}f} {suffix}{self.unit}/s", style="progress.data.speed")
+            if speed / unit >= 1
+            else Text(f"{unit / speed:,.{precision}f} {suffix}s/{self.unit}", style="progress.data.speed")
+        )
+
+
+def setup_prettier_tqdm() -> None:
+    """Set tqdm to use rich.
+
+    Fixes tqdm.rich rate column to support s/it when it/s < 1. Also, sets rate column floating point precision to 1.
+
+    """
+    tqdm_og.tqdm = tqdm_rich.tqdm
+    tqdm_rich.RateColumn = RateColumn
