3bf743bb0e501d52d24316ed4c36c5e946f76e69
alex-bene <alexandrosbene@gmail.com>
2025-12-12 01:40:26 +0100

    add opengl<->cv in `CoordinateConversions`

--- src/tinytools/threeD/coordinate_conversions.py
+++ src/tinytools/threeD/coordinate_conversions.py
@@ -1,7 +1,7 @@
 """Coordinate conversion utilities.
 
 This module provides matrices for converting between different 3D coordinate systems,
-such as OpenGL and PyTorch3D.
+such as OpenGL, OpenCV and PyTorch3D.
 """
 
 from __future__ import annotations
@@ -24,11 +24,11 @@ class CoordinateConversions(metaclass=FrozenNamespaceMeta):
             [0.0, 0.0, -1.0],  # Flip Z (forward to backward)
         ]
     )
-    pt3d_to_opengl = np.array(
+    opengl_to_cv = np.array(
         [
-            [-1.0, 0.0, 0.0],  # Flip X (left to right)
-            [0.0, 1.0, 0.0],
-            [0.0, 0.0, -1.0],  # Flip Z (backward to forward)
+            [1.0, 0.0, 0.0],
+            [0.0, -1.0, 0.0],  # Flip Y (top to bottom)
+            [0.0, 0.0, -1.0],  # Flip Z (forward to backward)
         ]
     )
     cv_to_pt3d = np.array(
@@ -38,10 +38,6 @@ class CoordinateConversions(metaclass=FrozenNamespaceMeta):
             [0.0, 0.0, 1.0],
         ]
     )
-    pt3d_to_cv = np.array(
-        [
-            [-1.0, 0.0, 0.0],  # Flip X (left to right)
-            [0.0, -1.0, 0.0],  # Flip Y (bottom to top)
-            [0.0, 0.0, 1.0],
-        ]
-    )
+    pt3d_to_cv = cv_to_pt3d
+    cv_to_opengl = opengl_to_cv
+    pt3d_to_opengl = opengl_to_pt3d
