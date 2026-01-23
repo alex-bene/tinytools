updates tests; pose eq fix; broadcast_postcompose fix

src/tinytools/threeD/pose_target.py
@@ -105,9 +105,11 @@ class InstancePose:  # noqa: PLW1641
             return NotImplemented
         return (
             torch.allclose(self.scale, other.scale, rtol=rtol, atol=atol)
-            and torch.allclose(self.rotation, other.rotation, rtol=rtol, atol=atol)
-            if self.rotation is not None
-            else other.rotation is None
+            and (
+                torch.allclose(self.rotation, other.rotation, rtol=rtol, atol=atol)
+                if self.rotation is not None and other.rotation is not None
+                else other.rotation is None and self.rotation is None
+            )
             and torch.allclose(self.translation, other.translation, rtol=rtol, atol=atol)
             and torch.allclose(self.scene_scale, other.scene_scale, rtol=rtol, atol=atol)
             and torch.allclose(self.scene_shift, other.scene_shift, rtol=rtol, atol=atol)
@@ -349,9 +351,11 @@ class PoseTarget:  # noqa: PLW1641
             return NotImplemented
         return (
             torch.allclose(self.scale, other.scale, rtol=rtol, atol=atol)
-            and torch.allclose(self.rotation, other.rotation, rtol=rtol, atol=atol)
-            if self.rotation is not None
-            else other.rotation is None
+            and (
+                torch.allclose(self.rotation, other.rotation, rtol=rtol, atol=atol)
+                if self.rotation is not None and other.rotation is not None
+                else other.rotation is None and self.rotation is None
+            )
             and torch.allclose(self.translation, other.translation, rtol=rtol, atol=atol)
             and torch.allclose(self.scene_scale, other.scene_scale, rtol=rtol, atol=atol)
             and torch.allclose(self.scene_center, other.scene_center, rtol=rtol, atol=atol)

src/tinytools/threeD/transforms.py
@@ -95,8 +95,9 @@ def broadcast_postcompose(
             post-composition.
 
     """
-    rotation_repr = None
+    rotation_repr = "mat"
     if rotation is None:
+        rotation_repr = None
         rotation = torch.eye(3, device=translation.device).expand(*translation.shape[:-1], 3, 3)
     if rotation.shape[-1] == 4:  # quaternion
         rotation = quaternion_to_matrix(rotation)

tests/test_pose_target.py
@@ -40,7 +40,7 @@ def test_pose_target_defaults() -> None:
     translation = torch.rand(5, 4, 2, 3) * 15
     pose_target = PoseTarget(translation=translation)
     pose_target_explicit = PoseTarget(
-        rotation=torch.eye(3).expand(5, 4, 2, 3, 3),
+        rotation=None,
         translation=translation,
         scale=torch.ones(5, 4, 2, 1),
         scene_center=torch.zeros(5, 4, 2, 3),
@@ -56,7 +56,7 @@ def test_instance_pose_defaults() -> None:
     translation = torch.rand(5, 4, 2, 3) * 15
     instance_pose = InstancePose(translation=translation)
     instance_pose_explicit = InstancePose(
-        rotation=torch.eye(3).expand(5, 4, 2, 3, 3),
+        rotation=None,
         translation=translation,
         scale=torch.ones(5, 4, 2, 1),
         scene_shift=torch.zeros(5, 4, 2, 3),